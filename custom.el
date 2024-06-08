bb469ae11fa41b177b901a84df814a717e0416fa
lucasvreis <lvreis@usp.br>
2024-06-08 16:00:24 -0300

    Setup desktop.el

--- custom.el
+++ custom.el
@@ -3,13 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
  '(safe-local-variable-values
-   '((prettify-symbols-unprettify-at-point quote right-edge)
-     (corfu-auto-prefix . 3)
+   '((corfu-auto-prefix . 3)
      (line-spacing . 5)
-     (jinx-languages . "en_US")
-     (jinx-local-words . "Borel Cantelli Donsker Donsker's Skorohod coboundary càdlàg ergodic summable"))))
+     (prettify-symbols-unprettify-at-point quote right-edge))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
